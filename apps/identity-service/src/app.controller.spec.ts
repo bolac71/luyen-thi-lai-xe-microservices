3011b17274a229f569c8faad604268a5a2140e73
QMinh <quangminhpham309@gmail.com>
2026-04-25 20:19:08 +0700

    fix: update to pass ci

--- apps/identity-service/src/app.controller.spec.ts
+++ apps/identity-service/src/app.controller.spec.ts
@@ -1,43 +1,31 @@
-import { beforeEach, describe, expect, it, jest } from '@jest/globals';
-import { Test, TestingModule } from '@nestjs/testing';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { PrismaService } from './prisma/prisma.service';
+import { Test, type TestingModule } from "@nestjs/testing";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { PrismaService } from "./prisma/prisma.service";
 
-describe('AppController', () => {
-  let appController: AppController;
-  const notiServiceMock = {
-    emit: jest.fn(),
-  };
-  const prismaServiceMock = {
-    $queryRaw: jest.fn(),
-    identityUser: {
-      upsert: jest.fn(),
-    },
-  };
+describe("AppController", () => {
+	let appController: AppController;
 
-  beforeEach(async () => {
-    const app: TestingModule = await Test.createTestingModule({
-      controllers: [AppController],
-      providers: [
-        AppService,
-        {
-          provide: 'NOTI_SERVICE',
-          useValue: notiServiceMock,
-        },
-        {
-          provide: PrismaService,
-          useValue: prismaServiceMock,
-        },
-      ],
-    }).compile();
+	beforeEach(async () => {
+		const module: TestingModule = await Test.createTestingModule({
+			controllers: [AppController],
+			providers: [
+				AppService,
+				{
+					provide: "NOTI_SERVICE",
+					useValue: {},
+				},
+				{
+					provide: PrismaService,
+					useValue: {},
+				},
+			],
+		}).compile();
 
-    appController = app.get<AppController>(AppController);
-  });
+		appController = module.get<AppController>(AppController);
+	});
 
-  describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
-    });
-  });
+	it('should return "Hello World!"', () => {
+		expect(appController.getHello()).toBe("Hello World!");
+	});
 });
