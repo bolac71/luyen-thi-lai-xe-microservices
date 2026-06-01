499315fb95b0d3f7134a0ccae040607645dee1af
ToanHuynh <huynhatoan0201@gmail.com>
2026-06-01 16:26:43 +0700

    test: test devops

--- apps/user-service/src/domain/exceptions/user-already-exists.exception.ts
+++ apps/user-service/src/domain/exceptions/user-already-exists.exception.ts
@@ -4,6 +4,6 @@ export class UserAlreadyExistsException extends DomainException {
   readonly code = 'USER_ALREADY_EXISTS';
 
   constructor(_email: string) {
-    super('Email already exists. (MSG10)');
+    super('Email already exists. (MSG10) haha');
   }
 }
